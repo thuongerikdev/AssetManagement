c9bdf2555ebb2cb02bbf630f9420642d48f4646b
Nguyễn Xuân Thưởng <121345028+thuongerikdev@users.noreply.github.com>
2026-05-16 15:31:44 +0700

    Update UserRepository.cs

--- TH.WebAPI/Service/Auth/TH.Auth.Infrastructure/Repository/User/UserRepository.cs
+++ TH.WebAPI/Service/Auth/TH.Auth.Infrastructure/Repository/User/UserRepository.cs
@@ -544,7 +544,12 @@ namespace TH.Auth.Infrastructure.Repository.User
                         avatar = u.profile.avatar,
                         gender = u.profile.gender,
                         dateOfBirth = u.profile.dateOfBirth
-                    }
+                    },
+                    roles = u.userRoles!.Select(ur => new RoleSlimDto
+                    {
+                        roleID = ur.role.roleID,
+                        roleName = ur.role.roleName
+                    }).ToList()
                 })
                 .ToListAsync(ct);
             return user;
